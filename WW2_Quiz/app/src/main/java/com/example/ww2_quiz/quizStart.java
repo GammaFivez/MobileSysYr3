33c6a760f3498a6f91fcf2b7ce294d5a457d50bd
GammaFivez <72508380+GammaFivez@users.noreply.github.com>
2022-01-24 23:58:25 +0000

    Ver1

---
+++ WW2_Quiz/app/src/main/java/com/example/ww2_quiz/quizStart.java
@@ -0,0 +1,14 @@
+package com.example.ww2_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quizStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_start);
+    }
+}
